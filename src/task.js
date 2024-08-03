509e4d27949de98ea41da6efd23e99d99e0fee59
Fernanda Alvarez <fernanda.alce2@gmail.com>
2024-08-02 18:54:16 -0600

    Estilos CSS, función agregar tareas, función traer tareas y función para visualizar tareas, UI.js

---
+++ src/task.js
@@ -0,0 +1,16 @@
+// Lista de tareas
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+// Funcion para llevar las tareas 
+export const getTasks = () => tasks;
+
+// Funcion para agregar una tarea
+export const addTask = (task) => {
+    const newTask = {
+        id: Date.now (),
+        text: task,
+        completed: false,
+    };
+    tasks.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(task));
+};
